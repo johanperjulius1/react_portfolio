3a9f72be32f64769117af5761b5529b25cbdeefd
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-03 17:22:59 +0200

    I am adding the first testfile, for if usercanseetheportfoliostructure. I am also smashing out the sceleton of the code for the first set of tests.

---
+++ cypress/integration/userCanSeePortfolioStructure.feature.js
@@ -0,0 +1,8 @@
+describe("Portfolio interface", () => {
+it("successfully renders", () => {
+cy.visit("http://localhost:3000");
+cy.get("#").should("","");
+cy.get("#").should("","");
+cy.get("#").should("","");
+});
+});
