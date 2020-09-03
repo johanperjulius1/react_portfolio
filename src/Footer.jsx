ef07d46b9dc10304e859add6270d95a8a4261d8f
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-03 20:21:50 +0200

    I am adding seperate jsx files for the header and footer. I am also adding implementation code for a header and a footer. My tests that check that my yarn server is up and running, a header, a footer and an Hello world elements is now also passing after I just the element content.

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2 id="footer">This Portfolio is Made with React 16.13.1</h2>
+      Made with React 16.13.1
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <h1 id="header">Johan Berglunds Portfolio</h1>
+    </nav>
+  );
+};
+
+export default Header; 
