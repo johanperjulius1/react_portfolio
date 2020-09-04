6d5e21cce9e308616451eb231755dc10f197ac28
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-04 13:32:17 +0200

    Push for errorcheck.

--- cypress/integration/userCanSeePortfolioStructure.feature.js
+++ cypress/integration/userCanSeePortfolioStructure.feature.js
@@ -1,7 +1,7 @@
 describe("Portfolio interface", () => {
 it("successfully renders", () => {
 cy.visit("http://localhost:3000");
-cy.get("#header").should("contain","Johan Berglunds Portfolio");
+cy.get("#header").should("contain","My Portfolio");
 cy.get("#footer").should("contain","Made with React 16.13.1");
 cy.get("#hello").should("contain","Hello World");
   });
