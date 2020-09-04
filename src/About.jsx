660a535b67b4e50992360c994c711a151887fe6a
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-04 11:12:00 +0200

    I am adding an About and a Projects page. I am also adding Switch and  router functionality.

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="ui main container">
+      <h1 id="about-header" className="ui header">About Me</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Suscipit fugiat saepe quis dolorum rerum obcaecati quas corporis omnis,
+        ullam delectus voluptates illo labore itaque, 
+        minus harum asperiores neque repellat! Ipsa.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Projects.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="ui main container">
+      <h1 id="projects-header" className="ui header">My Projects</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Suscipit fugiat saepe quis dolorum rerum obcaecati quas corporis omnis,
+        ullam delectus voluptates illo labore itaque, 
+        minus harum asperiores neque repellat! Ipsa.
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
