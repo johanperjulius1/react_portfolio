61fbbb1e34c9890c924e76be4d369bb68da4792a
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-06 23:51:58 +0200

    I am adding some implementation code to show my work experience under cv.

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -1,25 +1,23 @@
-import React from "react";
-import { UndrawDesignerLife } from "react-undraw-illustrations";
+import React, { Component } from "react";
 
-const Cv = () => {
-  return (
-    <div className="ui main container">
-      <div class="ui stackable two column grid">
-        <div class="column">
-          <UndrawDesignerLife primaryColor='#12283a' height='200px'/>
-        </div>
-      <div class="column">  
-          <h1 id="cv-header" className="ui header">Cv</h1>
-          <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Suscipit fugiat saepe quis dolorum rerum obcaecati quas corporis omnis,
-        ullam delectus voluptates illo labore itaque, 
-        minus harum asperiores neque repellat! Ipsa.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+class Cv extends Component {
+  state = {
+    projects: [
+      {
+        id:1,
+        name: "My First Job",
+      },
+      {
+        id:2,
+        name:"Equity Research",
+      },
+      {
+        id:3,
+        name:"Sales Analyst Team Leader",
+      },
+    ],
+  };
+}
+
 
 export default Cv;
---
+++ src/backupcv.jsx
@@ -0,0 +1,21 @@
+// import { UndrawDesignerLife } from "react-undraw-illustrations";
+// const Cv = () => {
+//   return (
+//     <div className="ui main container">
+//       <div class="ui stackable two column grid">
+//         <div class="column">
+//           <UndrawDesignerLife primaryColor='#12283a' height='200px'/>
+//         </div>
+//       <div class="column">  
+//           <h1 id="cv-header" className="ui header">Cv</h1>
+//           <p>
+//         Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+//         Suscipit fugiat saepe quis dolorum rerum obcaecati quas corporis omnis,
+//         ullam delectus voluptates illo labore itaque, 
+//         minus harum asperiores neque repellat! Ipsa.
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
