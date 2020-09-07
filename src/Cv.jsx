e737b634f63b725a854c6e6702d533a7ee8df57b
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-07 05:26:17 +0200

    I am succesfully creating the class Cv component to Cv.jsx . I am setting the initial state to hold a JSON object that I name projects. That name will change to names in the next version. The tests for the CV page are failing as I have not yet added the image element to the JSON object which I expect in the test.

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+
 class Cv extends Component {
   state = {
     projects: [
@@ -17,6 +18,30 @@ class Cv extends Component {
       },
     ],
   };
+
+  render() {
+    const projects = this.state.projects;
+    let projectsList;
+    
+    if (projects.length > 0) {
+      projectsList = projects.map((project) => {
+      return (
+        <div id={"job-" + project.id} key={project.id}>
+          <h3 className="ui header">{project.name}</h3>
+        </div>
+        );
+    });
+  }
+
+  return (
+    <div className="ui main container">
+    <h1 id="projects-header" className="ui header">
+    My jobs
+    </h1>
+    {projectsList}
+    </div>
+  );
+  }
 }
 
 
